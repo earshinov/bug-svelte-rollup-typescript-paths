f79811e7ab0aebac62eef5a454a0f6b16e7ee73c
Evgeny Arshinov <earshinov@gmail.com>
2023-05-04 16:12:29 +0200

    Initial revision

---
+++ src/Test.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { Route, Router } from 'svelte-navigator';
+
+  import Demos from './routes/Demos.svelte';
+  import Home from './routes/Home.svelte';
+  import NotFound from './routes/NotFound.svelte';
+  import Pts from './routes/Pts.svelte';
+  import NavLink from './ui/NavLink.svelte';
+</script>
+
+<Router>
+  <nav>
+    <NavLink to="pts">PTS</NavLink> |
+    <NavLink to="demos">Component demos</NavLink>
+  </nav>
+  <div>
+    <Route path="/" component={Home} />
+    <Route path="pts" component={Pts} />
+    <Route path="demos/*" component={Demos} />
+    <Route component={NotFound} />
+  </div>
+</Router>
+
+<style lang="scss">
+  @use '../style/variables' as vars;
+
+  nav {
+    margin-bottom: vars.$unit * 2;
+  }
+</style>
